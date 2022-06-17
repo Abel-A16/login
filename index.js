12d73e7434781399abe921ef7b50efa523ec0d46
Abel-A16 <zerubabelendale16@gmail.com>
2022-06-17 10:56:42 +0300

    add registration

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const mysql = require('mysql');
+var alert = require('alert')
+var Cryptr = require('cryptr');
 const express = require('express');
 const bodyParser = require('body-parser');
 const encoder = bodyParser.urlencoded();
@@ -28,11 +30,43 @@ app.post("/", encoder, (req,res) => {
       res.redirect("/welcome")
     } else {
       res.redirect("/");
+	alert("user name or password incorrect")
     }
     res.end()
   })
 })
+app.post("/register", encoder, (req,res) => {
+  var username = req.body.username
+  var userpass = req.body.userpass
+  var userpass2 = req.body.userpass2
+if(userpass == userpass2){
+ var sql = "INSERT INTO loginuser (user_name, user_pass) VALUES ?";
+  var values = [
+    [username, userpass],
+  ];
+  connection.query(sql, [values], function (err, result) {
+    if (err) throw err;
+   else { console.log("Number of records inserted: " + result.affectedRows);
+       res.redirect("/success")}
+
+  });
+}else {
+	res.redirect("/register")
+}
+});
+
 app.get("/welcome", (req, res) => {
   res.sendFile(__dirname + "/welcome.html");
 })
+
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/register.html");
+})
+
+
+app.get("/success", (req, res) => {
+  res.sendFile(__dirname + "/success.html");
+})
+
+
 app.listen(5000)
